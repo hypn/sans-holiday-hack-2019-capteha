939b86489a512d8a63d39b7ebdebd8978a8bf9b1
Ross Simpson <ross.simpson@gmail.com>
2019-12-26 18:28:24 +0200

    configured training script

--- FirstTraining.py
+++ FirstTraining.py
@@ -2,6 +2,6 @@ from imageai.Prediction.Custom import ModelTraining
 
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
-model_trainer.setDataDirectory("idenprof")
-model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.setDataDirectory(".")
+model_trainer.trainModel(num_objects=6, num_experiments=200, enhance_data=True, batch_size=4, show_network_summary=True)
 
